5f775d81747b03fc9d85bc425d1749b04030de81
Ahmed <ahmed4uashraf@gmail.com>
2026-03-21 13:57:01 +0200

    first-section-of-home-page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,16 @@
 import Image from "next/image";
-
+import vcsu from "../../public/vcsu.png"
 export default function Home() {
   return (
-   <></>
+   <div className="hero-section  ml-30 mr-30 flex justify-between">
+    <div><Image src={vcsu} alt="victoria college students' union logo" width={390} 
+        height={390} className="hover:rotate-6   transition-transform duration-300 "/> </div>
+        <div className="hero-section-text flex flex-col  justify-center items-center">
+
+          <h1 className="text-center text-8xl font-anton tracking-[30px]">Your Voice <br></br></h1>
+          <h2 className="text-center text-8xl font-anton leading-loose text-accent">Matters</h2>
+          <p className="font-league text-foreground text-4xl font-bold">CUNCTI . GENUS  . UNA . SUMUS</p>
+        </div>
+   </div>
   );
 }
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,14 +1,19 @@
 
 export default function NavBar(){
     return(
-        <nav className="m-4 flex justify-between">
-        <a href="" className="text-foreground font-anton text-3xl">VCSU</a>
-        <div className="links flex justify-between gap-25 text-foreground font-anton text-md">
-                    <a href="" >Our Legacy</a>
-                      <a href="" >Our Team</a>
-                        <a href="" className="text-accent">LogIn</a>
-                          <a href="" className="text-accent">SignUp</a>
-        </div>
+        <nav className="ml-6 mr-8 mt-4 mb-0   inset-x-0 flex justify-between z-10 ">
+        <a href="" className="text-accent font-anton text-3xl hover:text-foreground transition-colors duration-300">VCSU</a>
+        <div className="links flex justify-between gap-25 text-foreground font-anton text-md relative">
+                    <a href="" className="group relative" >Our Legacy                             <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-accent transition-all duration-300 group-hover:w-full"></span>
+</a>
+                      <a href="" className="group relative">Our Team  <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-accent transition-all duration-300 group-hover:w-full"></span></a>
+                        <a href="" className="text-accent group relative">LogIn                            
+                            <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full"></span>
+</a>
+                          <a href="" className="text-accent group relative ">SignUp
+                            <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full"></span>
+                          </a>
+                          </div>
         </nav>
     );
 }
